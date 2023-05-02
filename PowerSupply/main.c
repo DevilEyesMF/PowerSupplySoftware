a492c348835fad2778743f4fe521d60041852486
DevilEyesMF <remkootje.claes@gmail.com>
2023-05-02 16:09:18 +0200

    integerToASCII_5digits + formatValue

--- PowerSupply/main.c
+++ PowerSupply/main.c
@@ -22,7 +22,7 @@ int main(void)
 	setClockPrescaler1();
 	
 	/* Initialize IO */
-	IO_Init();
+	initIO();
 	
 	/* Initialize SPI */
 	SPI_MasterInit();
@@ -39,7 +39,7 @@ void setClockPrescaler1()
 	CLKPR = 0b00000000;
 }
 
-void IO_Init()
+void initIO()
 {
 	DDRA = 0x07;
 	DDRB = 0x00; // CS lijnen uitzoeken
@@ -60,4 +60,67 @@ void SPI_MasterTransmit(uint8_t data)
 	SPDR = data;
 	/* Wait for transmission complete */
 	while(!(SPSR & (1<<SPIF)));
+}
+
+void initDisplay()
+{
+	
+}
+
+void updateDisplay(uint16_t setVoltage, uint16_t measuredVoltage, uint16_t setCurrent, uint16_t measuredCurrent)
+{
+	/* Declare local char arrays for ASCII codes */
+	char c_setVoltage[6], c_measuredVoltage[6], c_setCurrent[6], c_measuredCurrent[6];
+	
+	/* Transform values to ASCII */
+	integerToASCII_5digits(setVoltage, c_setVoltage);
+	integerToASCII_5digits(measuredVoltage, c_measuredVoltage);
+	integerToASCII_5digits(setCurrent, c_setCurrent);
+	integerToASCII_5digits(measuredCurrent, c_measuredCurrent);
+	
+	/* Format the strings for the LCD */
+	formatValue(c_setVoltage);
+	formatValue(c_measuredVoltage);
+	formatValue(c_setCurrent);
+	formatValue(c_measuredCurrent);
+	
+	/* Print set voltage */
+
+	/* Print 2 spaces */
+	
+	/* Print measured voltage */
+	
+	/* new line */
+	
+	/* Print set current */
+	
+	/* Print 2 spaces */
+	
+	/* Print measured current */
+	
+}
+
+void integerToASCII_5digits(uint16_t number, char *c_number)
+{	
+	/* Separate the digits */
+    for (int i = 0, j = 10000; i < 5; i++, j/=10)
+    {
+	    c_number[i] = number / j + 0x30;
+	    number %= j;
+    }
+}
+
+void formatValue(char c[6])
+{
+	if (c[0] == '0')
+	{
+		c[0] = ' ';
+	}
+	
+	for (int i = 5; i >= 2; i--)
+	{
+		c[i+1] = c[i];
+	}
+	
+	c[2] = ',';
 }
--- PowerSupply/main.h
+++ PowerSupply/main.h
@@ -12,7 +12,11 @@
 
 /* prototype functions */
 void setClockPrescaler1(void);
-void IO_Init(void);
+void initIO(void);
 void SPI_MasterInit(void);void SPI_MasterTransmit(uint8_t data);
+void initDisplay(void);
+void updateDisplay(uint16_t, uint16_t, uint16_t, uint16_t);
+void integerToASCII_5digits(uint16_t, char*);
+void formatValue(char*);
 
 #endif /* MAIN_H_ */
